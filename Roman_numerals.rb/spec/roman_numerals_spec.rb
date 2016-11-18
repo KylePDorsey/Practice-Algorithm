cfad0b9558a3e32e390ec595b464355576bc2fcf
KylePDorsey <kylepdorsey@gmail.com>
2016-11-18 11:46:33 -0600

    Passing Test, Modern and Old Roman Numerals
    Error in rspec, expecting wrong outcome with 1234 old roman
    Error in Old Roman, ommited conversion of 10.

--- Roman_numerals.rb/spec/roman_numerals_spec.rb
+++ Roman_numerals.rb/spec/roman_numerals_spec.rb
@@ -18,46 +18,46 @@ describe 'converting an Arabic number to a Roman numeral' do
       expect(convert_to_roman(500)).to eq "D"
     end
         
-    it 'converts 1234 to MCCVVVVVVIIII' do
-      expect(convert_to_roman(1234)).to eq "MCCVVVVVVIIII"
+    it 'converts 1234 to MCCXXXIIII' do
+      expect(convert_to_roman(1234)).to eq "MCCXXXIIII"
     end
   end
 
   describe 'modern Roman numerals' do
     it 'converts 4 to IV' do
-      expect(convert_to_roman(4)).to eq "IV"
+      expect(convert_to_roman(4, {modern: true})).to eq "IV"
     end
 
     it 'converts 9 to IX' do
-      expect(convert_to_roman(9)).to eq "IX"
+      expect(convert_to_roman(9, {modern: true})).to eq "IX"
     end
 
     it 'converts 14 to XIV' do
-      expect(convert_to_roman(14)).to eq "XIV"
+      expect(convert_to_roman(14, {modern: true})).to eq "XIV"
     end
 
     it 'converts 44 to XLIV' do
-      expect(convert_to_roman(44)).to eq "XLIV"
+      expect(convert_to_roman(44, {modern: true})).to eq "XLIV"
     end
 
     it 'converts 99 to XCIX' do
-      expect(convert_to_roman(99)).to eq "XCIX"
+      expect(convert_to_roman(99, {modern: true})).to eq "XCIX"
     end
 
     it 'converts 400 to CD' do
-      expect(convert_to_roman(400)).to eq "CD"
+      expect(convert_to_roman(400, {modern: true})).to eq "CD"
     end
 
     it 'converts 944 to CMXLIV' do
-      expect(convert_to_roman(944)).to eq "CMXLIV"
+      expect(convert_to_roman(944, {modern: true})).to eq "CMXLIV"
     end
 
     it 'converts 1234 to MCCXXXIV' do
-      expect(convert_to_roman(1234)).to eq "MCCXXXIV"
+      expect(convert_to_roman(1234, {modern: true})).to eq "MCCXXXIV"
     end
 
     it 'converts 549 to DXLIX' do
-      expect(convert_to_roman(549)).to eq "DXLIX"
+      expect(convert_to_roman(549, {modern: true})).to eq "DXLIX"
     end
   end
 end
